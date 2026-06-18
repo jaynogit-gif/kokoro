c520666d18896fcd8e0763c55458cb37310ccf4b
jaynogit-gif <jaynogit@gmail.com>
2026-06-18 22:47:08 +0000

    Update kokoro-sript.js

--- kokoro-sript.js
+++ kokoro-sript.js
@@ -2,21 +2,31 @@ function changeSection(){
                 var selectMenu = document.getElementById("chapter-select");
                 var player = document.getElementById("book-player");
 
-                console.log("Switching audio to: " + selectMenu.value);
-
+                player.pause();
                 player.src = selectMenu.value;
                 player.load();
-                player.play();
 
-            }
+                var playPromise = player.play();
+                if(playPromise !== undefined) {
+                  playPromise.then(_ => {
+                    console.log("Stream verified and playing successfully!");
+                  }).caatch(error => {
+                    console.log("Browser safety trigger caught.Retrying stream manually.");
 
-            var player = document.getElementById("book-player");
-
-            player.addEventListener("ended", function() {
-                var selectMenu = document.getElementById("chapter-select")
-
-                if (selectMenu.selectedIndex < selectMenu.options.length - 1) {
-                    selectMenu.selectedIndex = selectMenu.selectedIndex + 1;
-                    changeSection();
+                    player.play()
+                  });
                 }
-            });
+
+
+
+
+var player = document.getElementById("book-player");
+player.addEventListener("ended", function() {
+  var selectMenu = document.getElementById("chapter-select");
+  if (selectMenu.selectedIndex < selectMenu.options.length - 1) {
+    selectMenu.selectedIndex = selectMenu.selectedIndex + 1;
+    changeSection();
+  }
+});
+
+
